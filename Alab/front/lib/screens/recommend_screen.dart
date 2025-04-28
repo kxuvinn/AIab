b7f50df8fd654abb7db554e4d70d1c9e494859d1
seonhyeong021 <ksh2120544@gmail.com>
2025-04-28 23:54:13 +0900

    Update recommend_screen.dart

--- Alab/front/lib/screens/recommend_screen.dart
+++ Alab/front/lib/screens/recommend_screen.dart
@@ -1,53 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 
-class RecommendScreen extends StatefulWidget {
+class RecommendScreen extends StatelessWidget {
   final String nickname;
   final String grade;
   final String subject;
 
-  RecommendScreen({required this.nickname, required this.grade, required this.subject});
-
-  @override
-  _RecommendScreenState createState() => _RecommendScreenState();
-}
-
-class _RecommendScreenState extends State<RecommendScreen> {
-  List<dynamic> problems = [];
-
-  Future<void> _loadProblems() async {
-    final response = await http.get(
-      Uri.parse('http://127.0.0.1:8000/recommend?grade=${widget.grade}&subject=${widget.subject}'),
-    );
-
-    if (response.statusCode == 200) {
-      final data = jsonDecode(response.body);
-      if (data['success']) {
-        setState(() {
-          problems = data['problems'];
-        });
-      }
-    }
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _loadProblems();
-  }
+  const RecommendScreen({
+    super.key,
+    required this.nickname,
+    required this.grade,
+    required this.subject,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('추천 문제')),
-      body: ListView.builder(
-        itemCount: problems.length,
-        itemBuilder: (context, index) {
-          return ListTile(
-            title: Text(problems[index]['text']),
-          );
-        },
+      appBar: AppBar(title: const Text('추천 문제 화면')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('닉네임: $nickname'),
+            Text('학년: $grade'),
+            Text('단원: $subject'),
+          ],
+        ),
       ),
     );
   }
